9db1ef636bc812da5d1a758dedb8f4b95bda6522
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-03-05 20:20:40 -0400

    funcial en columnas y filas

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const matriz = {
     ubicaciones: [], 
     numeros: []
 }
+function Limpiar_matriz() {
+    for (let i = 1; i < 10; i++) {
+        for (let j = 1; j < 10; j++) {
+            let celda = document.getElementById(i + "" + j)
+            celda.innerHTML = " "
+        }
+    }
+}
 function Crear_matriz(cantidad) {
     const ubicaciones = []
     const numeros = []
@@ -35,27 +43,53 @@ function Llenar_matriz() {
     }
 }
 function Depurar_matriz() {
+    let listaInical = []
+    let listaNumeral = []
     for (let i = 0; i < matriz.numeros.length; i++) {
         let inicial = matriz.ubicaciones[i]
         let numeral = matriz.numeros[i]
-        let listaInical = []
-        let listaNumeral = []
-        for (let j = 0; j < matriz.numeros.length; j++) {
-            let variable = matriz.ubicaciones[j]
-            if ((variable.startsWith(inicial[0]) || 
-            variable.endsWith(inicial[1])) && j != i) {
-                if (matriz.numeros[j] != numeral) {
-                    listaInical.push(variable)
-                    listaNumeral.push(matriz.numeros[j])   
+        let repetido = false
+        if (listaInical.length != 0) {
+            if (listaNumeral.includes(numeral)) {
+                for (let j = 0; j < listaInical.length; j++) {
+                    let variable = listaInical[j]
+                    if (variable.startsWith(inicial[0]) || 
+                        variable.endsWith(inicial[1])) {
+                        repetido = true
+                    }
                 }
-            } 
+                if (!repetido) {
+                    listaInical.push(inicial)
+                    listaNumeral.push(numeral)
+                }
+            } else {
+                listaInical.push(inicial)
+                listaNumeral.push(numeral)
+            }
+            
+        } else {
+            listaInical.push(inicial)
+            listaNumeral.push(numeral)
         }
-        console.log(listaInical)
-        console.log(listaNumeral)
+        // for (let j = 0; j < matriz.numeros.length; j++) {
+        //     let variable = matriz.ubicaciones[j]
+        //     if ((variable.startsWith(inicial[0]) || 
+        //     variable.endsWith(inicial[1])) && j != i) {
+        //         if (matriz.numeros[j] != numeral) {
+        //             listaInical.push(variable)
+        //             listaNumeral.push(matriz.numeros[j])   
+        //         }
+        //     } 
+        // }
     }
+    matriz.numeros = listaNumeral
+    matriz.ubicaciones = listaInical
+    console.log(listaInical)
+    console.log(listaNumeral)
     Llenar_matriz()
 }
 const facil = 38
+Limpiar_matriz()
 Crear_matriz(facil)
 Depurar_matriz()
 Llenar_matriz()
